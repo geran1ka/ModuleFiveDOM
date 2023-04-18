827ed1799673c5083d039c032a2c4b7341dc68cf
geran1ka <roman.khoruzhy@gmail.com>
2023-04-18 22:16:57 +0300

    Исправление ДЗ

--- you-dont-know-js/js/main.js
+++ you-dont-know-js/js/main.js
@@ -1,35 +1,34 @@
 'use strict';
 // получаем все карточки
 const cards = document.querySelectorAll('.item');
-
-// расположим card 1 после card 3
-cards[3].after(cards[0]);
-
-// получим все списки props__list
-const propsList = document.querySelectorAll('.props__list');
-
 // получим все элементы h2
 const itemsTitle = document.querySelectorAll('.item__title');
-
-// Замена глав заголовков
-propsList[5].before(itemsTitle[0]);
-propsList[1].before(itemsTitle[3]);
-propsList[4].before(itemsTitle[4]);
-
+// получим все списки props__list
+const propsList = document.querySelectorAll('.props__list');
 // перемещение глав
 const itemsTwo = document.querySelectorAll('.props__item_two');
 const itemsFour = document.querySelectorAll('.props__item_four');
-
+// Переменные для исправления первого замечания по отступу
+const itemTitleClone = itemsTitle[0].cloneNode();
+const itemTitleFirstCard = document.querySelector('[name = title-items]');
+// расположим card 1 после card 3
+cards[3].after(cards[0]);
+// Замена глав заголовков
+propsList[5].before(itemsTitle[1]);
+propsList[4].before(itemsTitle[4]);
+propsList[2].before(itemsTitle[3]);
+itemsTitle[2].textContent = 'This и прототипы объектов';
+// второй вариант встаки нового Заглавия
+// itemsTitle[2].innerHTML = 'This и прототипы объектов';
 // переместим главу 4 из 2-й карточуи в 4-ю
-itemsFour[3].after(itemsFour[0]);
-
+itemsFour[3].after(itemsFour[5]);
 // переместим приложения А/Б из 6-й карточуи в 2-ю
 itemsTwo[7].after(itemsTwo[8], itemsTwo[9]);
-
 // перемещение глав между 3 и 5 карточками;
-itemsTitle[1].after(propsList[4]);
-
-itemsTitle[4].after(propsList[2]);
-
+itemsTitle[2].after(propsList[4]);
+itemsTitle[4].after(propsList[3]);
 // удаляем баннер
 document.querySelector('.ads').remove();
+// вариант для исправления первого замечания по отступу
+itemTitleClone.textContent = 'Познакомьтесь, JavaScript';
+itemTitleFirstCard.replaceWith(itemTitleClone);
