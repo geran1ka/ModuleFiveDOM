ecf1ca2aa7f447fac8e3354bc2439a3fecf073f6
Geran1ka <roman.khoruzhy@gmail.com>
2023-04-19 14:28:38 +0300

    Get elements from modal window

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
 
-const html = document.documentElement;
-console.log(html.children);
+const modalTitle = document.querySelector('.modal__title');
+const modalButtonEddit = document.querySelector('.modal__button-eddit');
+const modalId = document.querySelector('.modal__id');
+// Можно здесь использовать const formAddProduct вместо modalForm
+const modalForm = document.querySelector('.modal__form');
+const formCheckboxDiscount = modalForm.querySelector('.form__checkbox-discount');
+const formInputDiccount = modalForm.querySelector('.form__input-diccount');
+const formTotalPrice = modalForm.querySelector('.form__total-price');
+
+console.log('modalTitle: ', modalTitle);
+console.log('modalButtonEddit: ', modalButtonEddit);
+console.log('modalId: ', modalId);
+console.log('modalForm: ', modalForm);
+console.log('formCheckboxDiscount: ', formCheckboxDiscount);
+console.log('formInputDiccount: ', formInputDiccount);
+console.log('formTotalPrice: ', formTotalPrice);
