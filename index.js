976b78c420dab245b9237ac03b2c97e570d57a19
geran1ka <roman.khoruzhy@gmail.com>
2023-04-20 22:17:19 +0300

    ready first task lesson_5_03

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 'use strict';
-
-let cmsList = document.querySelector('.cms__list');
-let game = true;
-
+const cmsList = document.querySelector('.cms__list');
+// функция запроса данных от пользователя
 const getUserData = () => prompt(`Введите текст для пункта списка`)?.trim();
-
-const print = (text) => {
+// функция создания элемента списка
+const getPageOutput = (text) => {
   const li = document.createElement('li');
   li.className = 'cms__item';
   li.textContent = text;
   return cmsList.append(li);
-}
-
+};
+// функция проверки
 const getResult = (text) => {
   switch (text) {
     case 'del':
-      return console.log('del');
+      cmsList.removeChild(cmsList.lastChild);
+      return;
     case 'clear':
       return cmsList.textContent = '';
     case '':
       return;
     default:
-      return print(text);
+      return getPageOutput(text);
   }
-}
+};
 
 const createList = (list) => {
   const text = getUserData();
+  console.log('text: ', text);
 
-  if (text === undefined) return;
+  if (text === undefined || text === 'exit') return;
 
   getResult(text);
-  
-  return createList (list);
-}
+
+  return createList(list);
+};
 
 createList();
+
