215b59639b54abe76ff421e2d573905939a355a5
geran1ka <roman.khoruzhy@gmail.com>
2023-06-10 20:13:00 +0300

    change name input discont => discount

--- script/function/totalPriceAllProduct.js
+++ script/function/totalPriceAllProduct.js
@@ -1,6 +1,6 @@
 export const totalPriceAllProduct = (arr) =>
   arr.reduce((acc, item) => (acc + (item.count * item.price -
-    item.count * item.price * (item.discont ? item.discont : 0) / 100)), 0);
+    item.count * item.price * (item.discount ? item.discount : 0) / 100)), 0);
 
 export const getTotalPricePage = (arr) => {
   const totalPricePage = document.querySelector('.text-price__all');
